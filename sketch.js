0adbf44c40a8d7e1b94cec984128fbc1a4d62169
Rafael-cod <69343624+Rafael-cod@users.noreply.github.com>
2020-10-24 17:51:31 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,15 +3,14 @@ var back_img;
 var gameState =0;
 var playerCount = 0;
 var allPlayers;
-
+var score =0;
 var player, form,game;
 var player1,player2;
 var players;
-var fruits;
-var fruitGroup;
+var fruits, fruitGroup;
 var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
 var player_img;
-
+var player1score =0, player2score =0;
 
 function preload(){
   back_img = loadImage("images/jungle.jpg");
